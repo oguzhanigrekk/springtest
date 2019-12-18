7fb2b97b43237de499d70102b1b1e71b3d2f8278
oguzhanigrekk <oguzhanigrek@gmail.com>
2019-12-18 17:23:39 +0300

    i

---
+++ src/main/java/com/osyy/testApplication.java
@@ -0,0 +1,14 @@
+package com.osyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class testApplication {
+
+	public static void main(String[] args) {
+
+         SpringApplication.run(testApplication.class, args);
+	}
+
+}
