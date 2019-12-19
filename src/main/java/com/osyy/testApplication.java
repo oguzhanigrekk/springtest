eae47cfb7b8bc299d356202136f0aff56c578965
oguzhanigrekk <oguzhanigrek@gmail.com>
2019-12-19 17:31:23 +0300

    1912

--- src/main/java/com/osyy/testApplication.java
+++ src/main/java/com/osyy/testApplication.java
@@ -2,13 +2,21 @@ package com.osyy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.osyyfile.prog.FileProperties;
 
 @SpringBootApplication
-public class testApplication {
+@EnableConfigurationProperties(value = FileProperties.class)
+public class TestApplication {
 
 	public static void main(String[] args) {
+		 
+		 FileProperties fp = new FileProperties() ;
+		 
 
-         SpringApplication.run(testApplication.class, args);
+         SpringApplication.run(TestApplication.class, args);
+         System.out.println(fp.getPath());
 	}
 
 }
